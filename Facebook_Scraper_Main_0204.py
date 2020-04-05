70f1222d5f26fa002e1e066af916e55deaa55170
Oliver Ward <ojward1995@gmail.com>
2020-04-05 18:24:17 +0700

    0504

--- Facebook_Scraper_Main_0204.py
+++ Facebook_Scraper_Main_0204.py
@@ -6,17 +6,22 @@ from collections import Counter
 from datetime import datetime
 from nltk import ngrams
 from pathlib import Path
+from Junk_words import remove_shit
+
 
 path_dir = os.path.join("C:\\", "Users", "Oliver", "Desktop",
-                        "Code_Files", "Facebook_Scraper", "JSON_Files", "Sam")
+                        "Code_Files", "Facebook_Scraper", "JSON_Files", "fam")
 
 
-#print (os.path.exists("C:/Users/Oliver/Desktop/Code_files/Facebook_scraper/JSON_Files"))
 json_files = [pos_json for pos_json in os.listdir(
     path_dir) if pos_json.endswith('json')]
 
 json_text_list = []
 jsondata2 = []
+total_participants= {}
+
+
+
 
 for index, js in enumerate(json_files):
     with open(os.path.join(path_dir, js), "r") as json_data:
@@ -47,31 +52,31 @@ Messages = (Info_to_Dict(jsondata2))
 
 
 def phrase_frequency(dicts):
-    freq = ''
-    for msg in range(1, len(dicts), 1):
-        if isinstance(dicts[msg][1], str) == True:
-            freq += (Messages[msg][1] + ' ')
+    global total_participants
 
-    freq_1 = ''
-    freq_2 = ''
-    for msg in range(1, len(dicts), 1):
-        if isinstance(dicts[msg][1], str) == True:
-            if (dicts[msg][0] == str('Oliver Ward')):
-                freq_1 += (Messages[msg][1] + ' ')
-            elif (dicts[msg][0] == str('Sam Well')):
-                freq_2 += (Messages[msg][1] + ' ')
-
-    Which_freq = freq
     print(""" 
-    Input the name of who's word/phrase frequency you want to see. 
+    Next, Input the name of who's word or phrase frequency you want to see. 
 
-    Incorrect input will result in showing  frequencies for all parties in the chat
+    Incorrect input will result in showing frequencies for all parties in the chat
     """)
-    participant = input().lower()
-    if participant == 'oliver':
-        Which_freq = freq_1
-    elif participant == 'sam':
-        Which_freq = freq_2
+    participant = input().lower()    
+    alls = ""
+
+
+    for i in range(1, len(dicts), 1):
+
+        if (participant in total_participants.keys()) and(isinstance(dicts[i][1], str) == True) and participant in Messages[i][0].lower():
+            total_participants[participant] += (Messages[i][1] + ' ')
+        elif participant not in total_participants.keys() and (isinstance(dicts[i][1], str) == True):
+            alls += (Messages[i][1] + ' ')
+    
+    if (participant not in total_participants.keys()):
+        Which_freq = alls
+    else:
+        Which_freq = total_participants[participant]
+
+    #print (Which_freq)
+
 
     print("""
     Do you want to see a list of all the most said words, or do you want to check how many times a 
@@ -85,7 +90,7 @@ def phrase_frequency(dicts):
     choice = int(input())
     n = 0
     p = ''
-    c = choice == 1
+    c = (choice == 1)
 
     if choice == 1:
         print("How many words do you want to see in a sequence? ")
@@ -97,15 +102,18 @@ def phrase_frequency(dicts):
     else:
         print("Incorrect entry. Please try again")
 
-    phrase_list = []
-    phrase_list_updated = []
-    bigrams = ngrams(Which_freq.split(), n)
-    for gram in bigrams:
-        phrase_list.append(str(gram))
+    def str_to_list(strs):
+        phrase_list = []
+        phrase_list_updated = []
+        bigrams = ngrams(strs.split(), n)
+        for gram in bigrams:
+            phrase_list.append(str(gram))
 
-    for i in phrase_list:
-        phrase_list_updated.append(i.replace("'", "").replace("(", '')
-                                   .replace(")", '').replace(",", ''))
+        for i in phrase_list:
+            phrase_list_updated.append(i.replace("'", "").replace("(", '')
+                                       .replace(")", '').replace(",", ''))
+
+        return phrase_list_updated
 
     def freq_count(lists):
         list_freq = {}
@@ -115,106 +123,133 @@ def phrase_frequency(dicts):
             else:
                 list_freq[item] = 1
 
-        if p != (''):
-            for i in sorted(list_freq, key=list_freq.get, reverse=True):
-                if p == i and n > 1:
-                    print("The phrase:", i, "Has been said",
-                          list_freq[i], "times.")
-                if p == i and n == 1:
-                    print("The word:", i, "Has been said",
-                          list_freq[i], "times.")
+        def display_list(dicts):
+            count = 0
+            for w in sorted(dicts, key=dicts.get, reverse=True):
+                if w not in remove_shit:
+                    print(w, ": ", dicts[w])
+                    count += 1
+                    if count == 25:
+                        break
 
-        from Junk_words import remove_shit
-        for w in sorted(list_freq, key=list_freq.get, reverse=True):
-            if c and (n == 1):
-                if list_freq[w] > 1000 and w not in remove_shit:
-                    print(w, ": ", list_freq[w])
-            elif c and (n == 2):
-                if list_freq[w] > 100:
-                    print(w, ": ", list_freq[w])
-            elif c and (n == 3):
-                if list_freq[w] > 25:
-                    print(w, ": ", list_freq[w])
-            elif c and (n == 4):
-                if list_freq[w] > 10:
-                    print(w, ": ", list_freq[w])
-            elif c and (n) >= 5:
-                if list_freq[w] > 2:
-                    print(w, ": ", list_freq[w])
+        def display_single(dicts):
+            if p != (''):
+                for i in sorted(dicts, key=dicts.get, reverse=True):
+                    if p == i and n > 1 and (i not in remove_shit):
+                        print("The phrase:", i, "Has been said",
+                              dicts[i], "times.")
+                    if p == i and n == 1 and (i not in remove_shit):
+                        print("The word:", i, "Has been said",
+                              dicts[i], "times.")
 
-    freq_count(phrase_list_updated)
+        if choice == 1:
+            display_list(list_freq)
+        elif choice == 2:
+            display_single(list_freq)
+
+    freq_count(str_to_list(Which_freq))
 
 
 def recipient_frequency(dicts):
+    global total_participants
     messages_freq = []
     for i in range(1, len(dicts), 1):
         messages_freq.append(dicts[i][0].split(' ')[0])
 
     def messages_freq_count(lists):
-        list_freq = {}
+        list_freq_x = {}
         for item in lists:
-            if (item in list_freq):
-                list_freq[item] += 1
+            if (item in list_freq_x):
+                list_freq_x[item] += 1
             else:
-                list_freq[item] = 0
+                list_freq_x[item] = 1
 
-    # when cleaning add this to a function
-        values = list(list_freq.values())
-        names = list(list_freq.keys())
-        values_ratio = []
-        valuess = []
+        def freq_disp(dicts):
+            global total_participants
+            values = list(dicts.values())
+            names = list(dicts.keys())
+            values_ratio = []
+            valuess = []
 
-        for i in values:
-            values_ratio.append(str((round(i/(sum(values))*100))))
-            valuess.append(str(i))
 
-        if len(names) == 2:
-            print("")
-            print(" and ".join(names), "have sent", " and ".join(
-                valuess), 'messages, Respectively.\n')
-            print("They would give them a message proportion of",
+            for i in values:
+                values_ratio.append(str((round(i/(sum(values))*100))))
+                valuess.append(str(i))
+            for i in names:
+                total_participants.update({str(i).lower():'x'})
+
+            if len(names) > 2:
+                print ("There are", len(names), "participants in the chat.")
+                print ("Below is a list of message frequency by recepient, in descending order: \n")
+                for i in sorted(list_freq_x, key=list_freq_x.get, reverse=True):
+                        print(i, ": ", list_freq_x[i])
+                                
+
+                print("\n This would give the participants", ", ".join(names[:-1]),"and", str(names[-1]) , " a message proportion of",
+                  ":".join(values_ratio), ", Respectively.\n _______________________________________")
+
+            elif len(names) == 2:
+                print("There are", len(names), "participants in the chat.")
+                print(" and ".join(names), "have sent", " and ".join(
+                   valuess), 'messages, Respectively.\n')
+                print("They would give them a message proportion of",
                   ":".join(values_ratio), ".\n")
 
+            #print(len(names))
+        freq_disp(list_freq_x)
     messages_freq_count(messages_freq)
 
+   
 
 def get_time(dicts):
-    times = []
-    timeset = ''
     print('\n press H for hour frequency, M for Month frequency or Y for Year frequency.\n')
     x = input().lower()
-    if x == 'h':
-        for msg in range(1, len(dicts), 1):
-            times.append((datetime.fromtimestamp(
-                int(str(dicts[msg][2])[:-3]))).hour)
-    elif x == 'm':
-        for msg in range(1, len(dicts), 1):
-            times.append((datetime.fromtimestamp(
-                int(str(dicts[msg][2])[:-3]))).month)
-    elif x == 'y':
-        for msg in range(1, len(dicts), 1):
-            times.append((datetime.fromtimestamp(
-                int(str(dicts[msg][2])[:-3]))).year)
-    else:
-        print('\n Not a valid entry. Please try again\n ')
 
-    def freq_count(lists):
-        list_freq = {}
-        for item in lists:
-            if (item in list_freq):
-                list_freq[item] += 1
-            else:
-                list_freq[item] = 1
+    def time_chooser(timez):
+        times = []
+        timeset = ''
+        if timez == 'h':
+            for msg in range(1, len(dicts), 1):
+                times.append((datetime.fromtimestamp(
+                    int(str(dicts[msg][2])[:-3]))).hour)
+        elif timez == 'm':
+            for msg in range(1, len(dicts), 1):
+                times.append((datetime.fromtimestamp(
+                    int(str(dicts[msg][2])[:-3]))).month)
+        elif timez == 'y':
+            for msg in range(1, len(dicts), 1):
+                times.append((datetime.fromtimestamp(
+                    int(str(dicts[msg][2])[:-3]))).year)
+        else:
+            print('\n Not a valid entry. Please try again\n ')
+        
+        def freq_count(lists):
+            list_freq = {}
+            for item in lists:
+                if (item in list_freq):
+                    list_freq[item] += 1
+                else:
+                    list_freq[item] = 0
 
-        for w in sorted(list_freq, key=list_freq.get, reverse=True):
-            if w >= 7:
-                print(w-7, ": ", list_freq[w])
-            if w < 7:
-                print(24-(7-w), ": ", list_freq[w])
+            def display(dicts):
+                for w in sorted(dicts, key=dicts.get, reverse=True):
+                    if timez == 'h':
+                        if w >= 7:
+                            print(w-7, ": ", dicts[w])
+                        if w < 7:
+                            print(24-(7-w), ": ", dicts[w])
+                    else:
+                        print(w, ":", dicts[w])
+            display(list_freq)
+        freq_count(times)
 
-    freq_count(times)
+    time_chooser(x)
 
-
+recipient_frequency(Messages)
 phrase_frequency(Messages)
-# recipient_frequency(Messages)
-# get_time(Messages)
+#get_time(Messages)
+#print (total_participants)
+
+
+
+
